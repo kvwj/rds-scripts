e0d85eeef2288195019f15ebf354039892f7ddda
Ken Andersen <ken.andersen@usu.edu>
2025-08-02 13:00:57 -0600

    Update zara_songname_to_rds.sh

--- zara_songname_to_rds.sh
+++ zara_songname_to_rds.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
+mkdir /mnt/zara
+mount -t cifs //${SERVER}/${FOLDER} -o user=${USER},pass=${PASS} /mnt/zara
+cd /opt/app
 #FILE="$1"
-FILE=/mnt/zara/CurrentSong.txt
+FILE=/mnt/zara/ZaraLogs/CurrentSong.txt
 #CMD="$2"
-CMD1=$(echo "TEXT=$(</mnt/zara/CurrentSong.txt )"|nc -w 2 192.168.0.108 7005)
-CMD2=$(echo "DPS=$(</mnt/zara/CurrentSong.txt )"|nc -w 2 192.168.0.108 7005)
+CMD1=$(echo "TEXT=$(</mnt/zara/ZaraLogs/CurrentSong.txt )"|nc -i 2 -w 2 192.168.0.108 7005)
+CMD2=$(echo "DPS=$(</mnt/zara/ZaraLogs/CurrentSong.txt )"|nc -i 2 -w 2 192.168.0.108 7005)
 LAST=`ls -l "$FILE"`
 while true; do
   sleep 1
   NEW=`ls -l "$FILE"`
   if [ "$NEW" != "$LAST" ]; then
-    echo "TEXT=$(</mnt/zara/CurrentSong.txt )"|nc -w 2 192.168.0.108 7005
-#    echo "DPS=$(</mnt/zara/CurrentSong.txt )"|nc -w 2 192.168.0.108 7005
+    SONGNAME=$(</mnt/zara/ZaraLogs/CurrentSong.txt)
+    NAMELENGTH=${#SONGNAME}
+    if [ "$NAMELENGTH" -gt 64 ]; then
+      SONGNAMECUT=$(echo $SONGNAME | cut -c 1-64)
+      echo "TEXT=$(echo $SONGNAMECUT )"|nc -i 2 -w 2 192.168.0.108 7005
+      echo "TEXT?"|nc -i 2 -w 2 192.168.0.108 7005
+      curl -X POST $SLACK_WEBHOOK_URL -d "{\"text\": \"KVWJ Song name too long -- '${SONGNAME}'\"}"
+    fi
+    if [ "$NAMELENGTH" -lt 65 ]; then
+      echo "TEXT=$(echo $SONGNAME )"|nc -i 2 -w 2 192.168.0.108 7005
+      echo "TEXT?"|nc -i 2 -w 2 192.168.0.108 7005
+    fi
+#    echo "DPS=$(</mnt/zara/ZaraLogs/CurrentSong.txt )"|nc -w 2 192.168.0.108 7005
     LAST="$NEW"
   fi
 done
